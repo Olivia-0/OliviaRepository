b0c94801efb3440ecb5274e1f484b222c7dfbd5f
ls <ls@bjpowernode.com>
2020-02-06 12:44:58 +0800

    初始提交,person里面只有一个id属性

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;/*
+ * ClassName:Person
+ * Package:com.bjpowernode.domain
+ * Description"TODO
+ * @date:2020/2/6 12:28
+ * @author:LS
+ */
+
+public class Person {
+
+    private String id;
+}
